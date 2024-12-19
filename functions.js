75032001eb3565519eb637943848613cf8be6469
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2024-12-19 16:35:45 +0000

    Create readLyrics and writeLyrics functions

---
+++ functions.js
@@ -0,0 +1,30 @@
+// We need a async function that can read the lyrics readLyrics()
+// Define the data variable to read the data from our taylor.json file and we will wait for this to be completed before executing the function
+// we want  to return data that is converted from JSON to JavaScript array. 
+// we want to use a try and catch statements for error handling 
+
+export async function readLyrics() {
+    try {
+      const data = await fs.readFile(taylor.json, "utf8");
+      return JSON.parse(data);
+    } catch (error) {
+      console.error("Error reading file:", error);
+      return null;
+    }
+  }
+  
+// We need a async function that can write an object into lyrics writeLyrics()
+//  write the data as a JSON formatted string into our taylor.json file and we will wait for this to be completed before executing the function
+
+// we want to return that this has been successful with a boolean statement that is true  
+// we want to use a try and catch statements for error handling 
+
+  export async function writeLyrics(data) {
+    try {
+      await fs.writeFile(taylor.json, JSON.stringify(data), "utf8");
+      return true;
+    } catch (error) {
+      console.error("Error writing file:", error);
+      return false;
+    }
+  }
--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const index = express();
 index.use(express.json()); // // Middleware to parse incoming JSON requests and make it available under req.bodyapp.use(express.json());
 
 const PORT = 5000;
+
+index.get("/", function (req, res) {
+    res.send("Welcome to the inspirational Taylor Swift quotes API");
+  });
+
+index.listen(PORT, function () {
+    console.log(`Server is now listening on http://localhost:${PORT}`);
+  });
+
